fcd9803aed205f28033f0344d5b2e734edc7da1d
Mark Gray <mark.gray@gamesys.co.uk>
2018-01-15 13:18:55 +0000

    sorting add task css and js

--- static/js/addtask.js
+++ static/js/addtask.js
@@ -1,6 +1,18 @@
 $(function(){
-	$('#btname').click(function(){
-		location.href="routename";		
+	$('#btnNextFields1').click(function(){
+		$('#primaryFields').hide(100);
+		$('#secondaryFields').show(100);
+		$('#ball2').css("background-color", "green");
+		$('#btnNextFields1').hide();
+		$('#btnNextFields2').show();
+	});
+
+	$('#btnNextFields2').click(function(){
+		$('#secondaryFields').hide(100);
+		$('#tertiaryFields').show(100);
+		$('#ball3').css("background-color", "green");
+		$('#btnNextFields2').hide();
+		$('#btnAddTask').show();
 	});
 
 	$('#killserver').click(function(){
@@ -17,8 +29,4 @@ $(function(){
 			}
 		});		
 	});
-	$('#skip-button').click(function(){
-		location.href="showHomePage";		
-	});
-
 });
