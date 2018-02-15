bbad3f5547d56435fd06eda5461e0c74421eb618
Mark Gray <mark.gray@gamesys.co.uk>
2018-02-15 13:32:36 +0000

    added some of the new add task page

--- static/js/addtask.js
+++ static/js/addtask.js
@@ -1,21 +1,4 @@
 $(function(){
-
-	$('#btnNextFields1').click(function(){
-		$('#primaryFields').hide();
-		$('#secondaryFields').show()
-		$('#ball2').css("background-color", "green");
-		$('#btnNextFields1').hide();
-		$('#btnNextFields2').show();
-	});
-
-	$('#btnNextFields2').click(function(){
-		$('#secondaryFields').hide();
-		$('#tertiaryFields').show();
-		$('#ball3').css("background-color", "green");
-		$('#btnNextFields2').hide();
-		$('#btnAddTask').show();
-	});
-
 	$('#btnAddTask').click(function(){
 		$.ajax({
 			url: '/addTask',
@@ -30,41 +13,5 @@ $(function(){
 		});
 
 		location.href="showTasks"
-	});
-
-	$('#ball1').click(function(){
-		$('#primaryFields').show();
-		$('#secondaryFields').hide();
-		$('#tertiaryFields').hide();
-		$('#ball1').css("background-color", "grey");
-		$('#ball2').css("background-color", "grey");
-		$('#ball2').css("background-color", "grey");
-		$('#btnNextFields1').show();
-		$('#btnNextFields2').hide();
-		$('#btnAddTask').hide();		
-	});
-
-	$('#ball2').click(function(){
-		$('#primaryFields').hide();
-		$('#secondaryFields').show();
-		$('#tertiaryFields').hide();
-		$('#ball1').css("background-color", "grey");
-		$('#ball2').css("background-color", "grey");
-		$('#ball2').css("background-color", "grey");
-		$('#btnNextFields1').hide();
-		$('#btnNextFields2').show();
-		$('#btnAddTask').hide();		
-	});
-
-	$('#ball3').click(function(){
-		$('#primaryFields').hide();
-		$('#secondaryFields').hide();
-		$('#tertiaryFields').show();
-		$('#ball1').css("background-color", "grey");
-		$('#ball2').css("background-color", "grey");
-		$('#ball2').css("background-color", "grey");
-		$('#btnNextFields1').hide();
-		$('#btnNextFields2').hide();
-		$('#btnAddTask').show();		
 	});
 });
