b7123a2c6c7c1ed9687a56d6a25768aff709f8ef
Mark <mfgray87@gmail.com>
2018-02-19 22:52:20 +0000

    various

--- static/js/tasks.js
+++ static/js/tasks.js
@@ -1,10 +1,4 @@
-function setClass(task){
-	if (task == "High") {
-		task.setAttribute("class", "task-I")
-		} else {
-		task.setAttribute("class", "task-nI")
-		};
-}
+
 
 function populateList(endpoint) {
   $.get(endpoint, function(data) {
@@ -25,30 +19,40 @@ function populateList(endpoint) {
 				task.setAttribute("class", "task nIU")
 				} else {
 				task.setAttribute("class", "task other")
-			};
-			var taskId = "task" + i			
-			task.setAttribute("id", taskId);
+			};			
 			task.innerHTML = results[i][0];
 			if (endpoint == "/getTasks") {
+				var taskId = "atask" + i			
+				task.setAttribute("id", taskId);
 				var startBtn = document.createElement("button");
-				var startId = "Start" + i
-				startBtn.setAttribute("class", "startBtn")
+				var startId = "astart" + i
+				startBtn.setAttribute("class", "btn start")
 				startBtn.setAttribute("id", startId);
-				startBtn.innerHTML = "Start"					
+				startBtn.innerHTML = "Start"
+				var attBtn = document.createElement("button");
+				var attBtnId = "attBtn" + i
+				attBtn.setAttribute("class", "btn att")
+				attBtn.setAttribute("id", attBtnId);
+				attBtn.innerHTML = "Add to Today"					
 				document.getElementById(allTasksList).appendChild(task);
 				document.getElementById(allTasksList).appendChild(startBtn);
+				document.getElementById(allTasksList).appendChild(attBtn);
 			} else if (endpoint == "/getInProgress") {
+				var taskId = "btask" + i			
+				task.setAttribute("id", taskId);
 				var pauseBtn = document.createElement("button");
 				var pauseId = "pause" + i
-				pauseBtn.setAttribute("class", "startBtn")
+				pauseBtn.setAttribute("class", "btn pause")
 				pauseBtn.setAttribute("id", pauseId);
 				pauseBtn.innerHTML = "Pause"					
 				document.getElementById(inProgressList).appendChild(task);
 				document.getElementById(inProgressList).appendChild(pauseBtn);
 			} 	else if (endpoint == "/getToDoToday") {
+				var taskId = "ctask" + i			
+				task.setAttribute("id", taskId);
 				var startBtn = document.createElement("button");
-				var startId = "Start" + i
-				startBtn.setAttribute("class", "startBtn")
+				var startId = "cstart" + i
+				startBtn.setAttribute("class", "btn start")
 				startBtn.setAttribute("id", startId);
 				startBtn.innerHTML = "Start"					
 				document.getElementById(todaysTaskList).appendChild(task);
@@ -60,12 +64,51 @@ function populateList(endpoint) {
 
 $( document ).ready(function() {
 	populateList("/getTasks");
-	console.log("It worked");
-	populateList("/getInProgress");
-	console.log("It worked too");    
+	populateList("/getInProgress");    
 	populateList("/getToDoToday");
-	console.log("Zomg no way, that too");
 });
+
+$(document).on("click",".start",function(){     		
+     		var id = this.id; 
+     		var index = id[6];
+     		if(id[0] == "a") {
+     			var taskId = "atask" + index;
+     		} else {
+     			var taskId = "ctask" + index;
+     		};     		
+     		var taskTitlehtml = document.getElementById(taskId);
+     		var taskTitle = taskTitlehtml.innerHTML;
+     		var data = {"title":taskTitle};
+			$.ajax({
+			    type: 'POST',
+			    contentType: 'application/json',
+			    url: '/startTask',
+			    dataType : 'json',
+			    data : JSON.stringify(data),
+			    success : function(result) {
+			      populateList("/getInProgress");
+			    },error : function(result){
+			       console.log("oops");
+			    }
+		});
+     		
+			/*var row = table.getElementsByTagName('tr')[rowId]
+			var titleCell = row.getElementsByTagName('td')[0]
+			var title = titleCell.innerHTML
+			var data = {"title":title};
+			$.ajax({
+			    type: 'POST',
+			    contentType: 'application/json',
+			    url: '/addToToday',
+			    dataType : 'json',
+			    data : JSON.stringify(data),
+			    success : function(result) {
+			      var resultText = result.text
+			    },error : function(result){
+			       console.log("oops");
+			    }
+		}); */
+	}); 
 
 
 			
