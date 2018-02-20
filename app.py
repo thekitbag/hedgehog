d26daae94d197a0a80573ca404cc23bd9276dda3
Mark Gray <mark.gray@gamesys.co.uk>
2018-02-20 10:07:11 +0000

    retrying

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ showToDoToday
 def getTasks():    
     conn = sqlite3.connect(db)
     c = conn.cursor()
-    c.execute("SELECT Title, Complexity FROM Tasks WHERE Status like 'Not Started'")
+    c.execute("SELECT Title, Urgency, Importance FROM Tasks WHERE Status like 'Not Started'")
     all_rows = c.fetchall()
     return jsonify(all_rows)
 
@@ -45,7 +45,7 @@ def getTasks():
 def getInProgress():    
     conn = sqlite3.connect(db)
     c = conn.cursor()
-    c.execute("SELECT Title FROM Tasks WHERE Status like 'In Progress'")
+    c.execute("SELECT Title, Urgency, Importance FROM Tasks WHERE Status like 'In Progress'")
     all_rows = c.fetchall()
     return jsonify(all_rows)
 
@@ -61,7 +61,7 @@ def getHardDeadlines():
 def getToDoToday():    
     conn = sqlite3.connect(db)
     c = conn.cursor()
-    c.execute("SELECT Title FROM Tasks WHERE Status like 'To Do Today'")
+    c.execute("SELECT Title, Importance, Urgency FROM Tasks WHERE Status like 'To Do Today'")
     all_rows = c.fetchall()
     return jsonify(all_rows)
 
--- static/js/tasks.js
+++ static/js/tasks.js
@@ -1,3 +1,5 @@
+
+
 function populateList(endpoint) {
   $.get(endpoint, function(data) {
   	var results = data;  
@@ -7,30 +9,50 @@ function populateList(endpoint) {
     var todaysTaskList = "todaysTasksContainer";
     for (var i = 0; i < resultsLength; i++) {			
 			var task = document.createElement("div");
-			var taskId = "task" + i
-			task.setAttribute("class", "task-nInU")
-			task.setAttribute("id", taskId);
+			if (results[i][1] == "High" && results[i][2] == "High") {
+				task.setAttribute("class", "task IU")
+				} else if (results[i][1] == "Low" && results[i][2] == "Low"){
+				task.setAttribute("class", "task nInU")
+				} else if (results[i][1] == "Low" && results[i][2] == "High"){
+				task.setAttribute("class", "task InU")
+				} else if (results[i][1] == "High" && results[i][2] == "Low"){
+				task.setAttribute("class", "task nIU")
+				} else {
+				task.setAttribute("class", "task other")
+			};			
 			task.innerHTML = results[i][0];
 			if (endpoint == "/getTasks") {
+				var taskId = "atask" + i			
+				task.setAttribute("id", taskId);
 				var startBtn = document.createElement("button");
-				var startId = "Start" + i
-				startBtn.setAttribute("class", "startBtn")
+				var startId = "astart" + i
+				startBtn.setAttribute("class", "btn start")
 				startBtn.setAttribute("id", startId);
-				startBtn.innerHTML = "Start"					
+				startBtn.innerHTML = "Start"
+				var attBtn = document.createElement("button");
+				var attBtnId = "attBtn" + i
+				attBtn.setAttribute("class", "btn att")
+				attBtn.setAttribute("id", attBtnId);
+				attBtn.innerHTML = "Add to Today"					
 				document.getElementById(allTasksList).appendChild(task);
 				document.getElementById(allTasksList).appendChild(startBtn);
+				document.getElementById(allTasksList).appendChild(attBtn);
 			} else if (endpoint == "/getInProgress") {
+				var taskId = "btask" + i			
+				task.setAttribute("id", taskId);
 				var pauseBtn = document.createElement("button");
 				var pauseId = "pause" + i
-				pauseBtn.setAttribute("class", "startBtn")
+				pauseBtn.setAttribute("class", "btn pause")
 				pauseBtn.setAttribute("id", pauseId);
 				pauseBtn.innerHTML = "Pause"					
 				document.getElementById(inProgressList).appendChild(task);
 				document.getElementById(inProgressList).appendChild(pauseBtn);
 			} 	else if (endpoint == "/getToDoToday") {
+				var taskId = "ctask" + i			
+				task.setAttribute("id", taskId);
 				var startBtn = document.createElement("button");
-				var startId = "Start" + i
-				startBtn.setAttribute("class", "startBtn")
+				var startId = "cstart" + i
+				startBtn.setAttribute("class", "btn start")
 				startBtn.setAttribute("id", startId);
 				startBtn.innerHTML = "Start"					
 				document.getElementById(todaysTaskList).appendChild(task);
@@ -42,12 +64,51 @@ function populateList(endpoint) {
 
 $( document ).ready(function() {
 	populateList("/getTasks");
-	console.log("It worked");
-	populateList("/getInProgress");
-	console.log("It worked too");    
+	populateList("/getInProgress");    
 	populateList("/getToDoToday");
-	console.log("Zomg no way, that too");
 });
+
+$(document).on("click",".start",function(){     		
+     		var id = this.id; 
+     		var index = id[6];
+     		if(id[0] == "a") {
+     			var taskId = "atask" + index;
+     		} else {
+     			var taskId = "ctask" + index;
+     		};     		
+     		var taskTitlehtml = document.getElementById(taskId);
+     		var taskTitle = taskTitlehtml.innerHTML;
+     		var data = {"title":taskTitle};
+			$.ajax({
+			    type: 'POST',
+			    contentType: 'application/json',
+			    url: '/startTask',
+			    dataType : 'json',
+			    data : JSON.stringify(data),
+			    success : function(result) {
+			      populateList("/getInProgress");
+			    },error : function(result){
+			       console.log("oops");
+			    }
+		});
+     		
+			/*var row = table.getElementsByTagName('tr')[rowId]
+			var titleCell = row.getElementsByTagName('td')[0]
+			var title = titleCell.innerHTML
+			var data = {"title":title};
+			$.ajax({
+			    type: 'POST',
+			    contentType: 'application/json',
+			    url: '/addToToday',
+			    dataType : 'json',
+			    data : JSON.stringify(data),
+			    success : function(result) {
+			      var resultText = result.text
+			    },error : function(result){
+			       console.log("oops");
+			    }
+		}); */
+	}); 
 
 
 			
